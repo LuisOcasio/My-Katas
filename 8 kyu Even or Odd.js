168c33fa52a7f361669590a42c89639266fefd01
Luis Ocasio <luisocasio03@gmail.com>
2019-07-20 12:33:56 -0400

    //Solution-Using ternary operator

--- 8 kyu Even or Odd.js
+++ 8 kyu Even or Odd.js
@@ -1,8 +1,5 @@
 //Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 //Solution-Using ternary operator
-even_or_odd = number => {
-    return number % 2 === 0 ? "Even" : number % 2 === 1 ? "Odd" : "Odd";
-};
-
+const even_or_odd = n => (n & 1 ? "Odd" : "Even");
 console.log(even_or_odd());
